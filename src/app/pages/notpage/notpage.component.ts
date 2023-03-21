e43773df519d913e81211b8e69b3d575b7b3d4b5
Montes Antonio <omontes@indelpro.com>
2023-03-21 12:34:13 -0600

    1.Estructura base de componentes del proyecto

---
+++ src/app/pages/notpage/notpage.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notpage',
+  templateUrl: './notpage.component.html',
+  styleUrls: ['./notpage.component.css'],
+})
+export class NotPageComponent {}
